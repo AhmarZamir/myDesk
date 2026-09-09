327ab41cb13bb03a8955766c3d786548bae306e3
Ahmar Zamir <mangiahmar2016@gmail.com>
2026-09-09 06:14:17 +0500

    Handle password recovery sessions

--- lib/auth/auth_gate.dart
+++ lib/auth/auth_gate.dart
@@ -1,20 +1,49 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../app/app_shell.dart';
 import 'auth_screen.dart';
+import 'reset_password_screen.dart';
 
-class AuthGate extends StatelessWidget {
+class AuthGate extends StatefulWidget {
   const AuthGate({super.key});
 
   @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  late final StreamSubscription<AuthState> _subscription;
+  bool _passwordRecovery = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _subscription = Supabase.instance.client.auth.onAuthStateChange.listen((state) {
+      if (!mounted) return;
+      if (state.event == AuthChangeEvent.passwordRecovery) {
+        setState(() => _passwordRecovery = true);
+      } else if (state.event == AuthChangeEvent.signedOut) {
+        setState(() => _passwordRecovery = false);
+      } else {
+        setState(() {});
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return StreamBuilder<AuthState>(
-      stream: Supabase.instance.client.auth.onAuthStateChange,
-      builder: (context, snapshot) {
-        final session = Supabase.instance.client.auth.currentSession;
-        if (session == null) return const AuthScreen();
-        return const AppShell();
-      },
-    );
+    final session = Supabase.instance.client.auth.currentSession;
+    if (session == null) return const AuthScreen();
+    if (_passwordRecovery) {
+      return ResetPasswordScreen(onDone: () => setState(() => _passwordRecovery = false));
+    }
+    return const AppShell();
   }
 }
